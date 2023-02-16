c2927e8481f8595e9fb8297e3a0fd60d490ceece
Michiel Hegemans <michiel@splinterlands.com>
2023-02-16 14:23:17 +0100

    Convert external bridge to allow multiple endpoints

--- test.js
+++ test.js
@@ -1,21 +1,47 @@
-const bridge = require('./external-bridge');
-
-start();
+const Operations = require('./operations');
+const ExternalBridge = require("./external-bridge");
 
 async function start() {
-	bridge.init({ 
-		logging_level: 4, 
-		game_api_url: "http://localhost:3000", 
-		prefix: "matt-sm_",
+    const bridge = new ExternalBridge({
+        games: [
+            {
+                name: 'splinterlands',
+                api_url: "http://localhost:3000",
+                cb: onTx,
+                types: () => true,
+                state_file_name: 'sl-state.json',
+            },
+			{
+				name: 'validator',
+				api_url: "http://localhost:3340",
+				cb: onTx,
+				types: () => true,
+				state_file_name: 'v-state.json',
+				is_validator: true,
+			}
+        ],
 		api: {
 			port: 3100,
-			get_status: () => { return { chain: 'test', last_updated: new Date() }; }
+			external_status: () => ({ chain: 'test', last_updated: new Date() }),
 		}
+    })
+
+	Operations.init({
+		logging_level: 4,
+		prefix: "matt-sm_",
 	});
 
-	bridge.stream(onTx);
+	bridge.stream();
+
+	process.on('SIGINT', async () => {
+		await bridge.stop();
+		// Exit program as hive interface has an uninterruptible setTimeout running.
+		process.exit(0);
+	});
 }
 
 async function onTx(tx) {
-	console.log(tx);
+    console.log(tx);
 }
+
+start();
--- utils.js
+++ utils.js
@@ -6,18 +6,79 @@ function set_options(options) {
 	_options = Object.assign(_options, options);
 }
 
-async function getHeadBlockNum() {
-	const res = await axios.get(`${_options.game_api_url}/last_block}`);
+/**
+ * Fetches the head block number of the validator
+ * @params url {string} Url of the game api
+ * @returns {Promise<number | undefined>}
+ */
+async function getHeadBlockNumValidator(url) {
+	const res = await axios.get(`${url}/status`);
 
 	if (res.data && res.data.last_block) {
-		return res.data.last_block;
+		const last_block = parseInt(res.data.last_block, 10);
+		return isNaN(last_block) ? undefined : last_block;
 	}
 
 	throw new Error(null);
 }
 
-async function getBlock(block_num) {
-	const res = await axios.get(`${_options.game_api_url}/transactions/by_block`, { params: { block: block_num }});
+/**
+ * Fetches the head block number of the block processor
+ * @params url {string} Url of the game api
+ * @returns {Promise<number | undefined>}
+ */
+async function getHeadBlockNum(url) {
+	const res = await axios.get(`${url}/last_block`);
+
+	if (res.data && res.data.last_block) {
+		const last_block = parseInt(res.data.last_block, 10);
+		return isNaN(last_block) ? undefined : last_block;
+	}
+
+	throw new Error(null);
+}
+
+/**
+ * @typedef {{
+ *     id: string,
+ *     block_id: string,
+ *     prev_block_id: string,
+ *     type: string,
+ *     player: string,
+ *     data: string,
+ *     success: boolean,
+ *     error: null | string,
+ *     block_num: number,
+ *     created_date: string,
+ *     result: string,
+ * }} Transaction
+ */
+
+
+/**
+ * Fetches the specified block from the block processor
+ * @param url {string} Url of the game api
+ * @param block_num {number}
+ * @returns {Promise<Transaction[]>}
+ */
+async function getBlockValidator(url, block_num) {
+	const res = await axios.get(`${url}/transactions/${block_num}`);
+
+	if (res.data && Array.isArray(res.data)) {
+		return res.data;
+	}
+
+	throw new Error(null);
+}
+
+/**
+ * Fetches the specified block from the block processor
+ * @param url {string} Url of the game api
+ * @param block_num {number}
+ * @returns {Promise<Transaction[]>}
+ */
+async function getBlock(url, block_num) {
+	const res = await axios.get(`${url}/transactions/by_block`, { params: { block: block_num }});
 
 	if (res.data && Array.isArray(res.data)) {
 		return res.data;
@@ -85,6 +146,8 @@ module.exports = {
 	timeout,
 	tryParse,
 	getCurrency,
+	getHeadBlockNumValidator,
 	getHeadBlockNum,
-	getBlock
+	getBlockValidator,
+	getBlock,
 }
